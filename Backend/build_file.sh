1f1b0f108e52111bb76a52e36fadc6f27b2440d6
ezecon <nationhexus@gmail.com>
2024-10-08 22:32:54 +0600

    updated

--- Backend/build_file.sh
+++ Backend/build_file.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Install required dependencies
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Apply migrations
-python manage.py migrate
+python3 manage.py migrate
